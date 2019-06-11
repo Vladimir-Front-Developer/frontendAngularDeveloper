56c747e6e114bf12a39fe240618be78d2b35c3b1
Yastrebov V.S <wladimirtlt@yandex.ru>
2019-06-12 02:16:45 +0400

    Worked with animation,

--- src/app/contents/notebook/notebook.component.ts
+++ src/app/contents/notebook/notebook.component.ts
@@ -16,10 +16,9 @@ export interface pageData {
 
 export class NotebookComponent {
   @Input() nameNotebook: string
-  currentPage: number = 0
   pageData: pageData[] = [
     {
-      id: 1,
+      id: 3,
       headline: 'JavaScript',
       subtitle: 'AngularCLI 7+',
       percent: 40,
@@ -33,19 +32,42 @@ export class NotebookComponent {
       text: 'Какое то описание по поводу навыка',
     },
     {
-      id: 3,
+      id: 1,
       headline: 'html',
       subtitle: '',
       percent: 100,
       text: 'Какое то описание по поводу навыка',
     },
   ]
+  currentPage: number = 1
 
-  nextPage(id: number){
-    const lenPD = this.pageData.length
+  nextPage(){
     this.currentPage++
-    if(lenPD < this.currentPage) this.currentPage = 0
-    console.log(this.currentPage)
+    if(this.currentPage > this.pageData.length) this.currentPage = 1
   }
 
 }
+// pageAnimate: string = 'closed'
+// [@pageTurning]="pageAnimate"
+// this.pageAnimate = 'open'
+// *ngIf="currentPage === page.id"
+// animations: [
+//   trigger('pageTurning', [
+//     state('closed', style({ 'backface-visibility': 'visible' })),
+//     state('open', style({ 'backface-visibility': 'hidden' })),
+//     transition('closed => open', [
+//       animate('2s ease-in', keyframes([
+//         style({'backface-visibility': 'visible'}),
+//         style({'backface-visibility': 'hidden'})
+//      ]))
+//     ]),
+//   ])
+// ]
+// import {
+//   trigger,
+//   state,
+//   style,
+//   animate,
+//   transition,
+//   keyframes,
+// } from '@angular/animations';
--- src/app/contents/notebook/notebook.module.ts
+++ src/app/contents/notebook/notebook.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { NotebookComponent } from './notebook.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     NotebookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   exports: [
     NotebookComponent
